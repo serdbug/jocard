6a8878daa989e79777b0c0378f3e0af700c7e676
Jonatan Alfredo <107369692+serdbug@users.noreply.github.com>
2023-09-26 03:18:23 -0300

    ooo

---
+++ oscuro.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Evento clic para cambiar al modo oscuro
+    $('#oscuro').click(function() {
+        // Toggle de la clase dark-mode en el body
+        $('body').toggleClass('dark-mode');
+    });
+});
